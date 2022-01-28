72ebf15fb2ccb3470d58d5103891bab9aeeadacc
vladtymo <tymo.vlad@gmail.com>
2022-01-28 16:52:30 +0200

    Add project files.

---
+++ EF_Migrations/CinemaContext.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EF_Migrations
+{
+    public class CinemaContext : DbContext
+    {
+        public CinemaContext()
+        {
+            //Database.EnsureDeleted();
+            //Database.EnsureCreated();
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+
+            optionsBuilder.UseSqlServer(@"Data Source=DESKTOP-O0M8V28\SQLEXPRESS;Initial Catalog=CinemaDb;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+        }
+
+        public DbSet<Movie> Movies { get; set; }
+        public DbSet<Session> Sessions { get; set; }
+        public DbSet<Ticket> Tickets { get; set; }
+        public DbSet<Actor> Actors { get; set; }
+    }
+
+    public class Ticket
+    {
+        public int Id { get; set; }
+        public int Row { get; set; }
+        public int Place { get; set; }
+        public decimal Price { get; set; }
+        [Required]
+        public Session Session { get; set; }
+    }
+
+    public class Session
+    {
+        public int Id { get; set; }
+        public int Places { get; set; }
+        public bool Is3D { get; set; }
+        public DateTime Time { get; set; }
+        [Required]
+        public Movie Movie { get; set; }
+        public ICollection<Ticket> Tickets { get; set; }
+    }
+
+    public class Movie
+    {
+        public int Id { get; set; }
+        [Required, MaxLength(300)]
+        public string Title { get; set; }
+        public int Year { get; set; }
+        public string Genre { get; set; }
+        //public TimeSpan Duration { get; set; }
+        public float Rating { get; set; }
+        public ICollection<Actor> Actors { get; set; }
+        public ICollection<Session> Sessions { get; set; }
+    }
+
+    public class Actor
+    {
+        public int Id { get; set; }
+        [Required, MaxLength(100)]
+        public string FirstName { get; set; }
+        [Required, MaxLength(100)]
+        public string LastName { get; set; }
+        public DateTime Birthdate { get; set; }
+        public ICollection<Movie> Movies { get; set; }
+    }
+}
---
+++ EF_Migrations/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace EF_Migrations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CinemaContext context = new CinemaContext();
+
+            //ShowMovies(context.Movies);
+            //ShowMovies(context.Movies.Where(m => m.Rating >= 7).OrderByDescending(m => m.Rating));
+
+            ShowMovies(context.Movies.Where(m => m.Year >= DateTime.Now.Year - 5));
+        }
+        static void ShowMovies(IEnumerable<Movie> movies)
+        {
+            foreach (var m in movies)
+            {
+                Console.WriteLine($"-=-=-=-=-=- {m.Title} {m.Year} -=-=-=-=-=-\n" +
+                    $"Genres: {m.Genre}\n" +
+                    $"Rating: {m.Rating}\n");
+                    //$"Actors: {m.Actors.Count}");
+            }
+        }
+    }
+}
