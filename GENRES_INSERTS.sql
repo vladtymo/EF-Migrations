e2dd186a89f5789b756afe60debb0a6e8eed3fb3
vladtymo <tymo.vlad@gmail.com>
2022-01-31 16:14:12 +0200

    Rename SQL Files

---
+++ GENRES_INSERTS.sql
@@ -0,0 +1,11 @@
+﻿insert into Genres (Name)
+values	('Comedy'),
+		('Crime'),
+		('Film-Noir'),
+		('Mystery'),
+		('Romance'),
+		('Thriller'),
+		('Drama'),
+		('Musical'),
+		('War Horror'),
+		('Adventure');
